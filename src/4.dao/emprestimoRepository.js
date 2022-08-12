4464e4f237a8695dc82cb1ac0986be8f5396b97a
RomuloAlbuquerque <romulo.albuquerque.dev@gmail.com>
2022-08-11 23:56:27 -0300

    refatoração

--- src/4.dao/emprestimoRepository.js
+++ src/4.dao/emprestimoRepository.js
@@ -9,23 +9,29 @@ const carteiraRepository = new CarteiraRepository();
 class EmprestimoRepository{
 
     async emprestar(valor, quantParcelas, cpf){
+        const [{carteira_id}] = await configBanco.banco.query(
+            `select carteira_id from carteira where carteira_cpf = $1`,
+            {bind: [cpf],
+            type: QueryTypes.SELECT}
+        );
+        console.log("::::::::::",carteira_id);
         const emprestimo = new Extrato(parseFloat(valor), parseInt(quantParcelas), cpf);
-        await configBanco.banco.query(
-            `insert into emprestimo(emprestimo_cpf, emprestimo_valor, emprestimo_quant_parcelas) values($1, $2, $3) returning *`,
-            {bind: [emprestimo.cpf, emprestimo.valorEmprestado, emprestimo.quantParcelas], 
+        const [[{emprestimo_id}]] = await configBanco.banco.query(
+            `insert into emprestimo(emprestimo_cpf, emprestimo_valor, emprestimo_quant_parcelas, carteira_id) values($1, $2, $3, $4) returning emprestimo_id`,
+            {bind: [emprestimo.cpf, emprestimo.valorEmprestado, emprestimo.quantParcelas, carteira_id], 
             type: QueryTypes.INSERT}
         );
         await configBanco.banco.query(
-            `insert into extrato(extrato_cpf, extrato_total_pagar, extrato_quant_parcelas, extrato_valor_parcela, extrato_valor_emprestado, extrato_total_juros, extrato_taxa_juros_mensal) values($1, $2, $3, $4, $5, $6, $7)`,
-            {bind: [emprestimo.cpf, emprestimo.totalPagar, emprestimo.quantParcelas, emprestimo.valorParcela, emprestimo.valorEmprestado, emprestimo.totalJuros, emprestimo.taxaJurosMensal], 
+            `insert into extrato(extrato_cpf, extrato_total_pagar, extrato_quant_parcelas, extrato_valor_parcela, extrato_valor_emprestado, extrato_total_juros, extrato_taxa_juros_mensal, emprestimo_id) values($1, $2, $3, $4, $5, $6, $7, $8)`,
+            {bind: [emprestimo.cpf, emprestimo.totalPagar, emprestimo.quantParcelas, emprestimo.valorParcela, emprestimo.valorEmprestado, emprestimo.totalJuros, emprestimo.taxaJurosMensal, emprestimo_id], 
             type: QueryTypes.INSERT}
         );
         await carteiraRepository.depositar(valor, cpf);
         const {parcelas} = emprestimo;
-        for(let i=1; i<emprestimo.parcelas.length; i++){
+        for(let i=0; i<emprestimo.parcelas.length; i++){
             await configBanco.banco.query(
-                `insert into parcela(parcela_cpf, valor_parcela, data_emprestimo, data_vencimento, numero_parcela, quant_parcelas) values($1, $2, $3, $4, $5, $6)`,
-                {bind: [parcelas[i].cpf, parcelas[i].valorParcela, parcelas[i].dataEmprestimo, parcelas[i].dataVencimento, parcelas[i].numeroParcela, parcelas[i].quantParcelas], 
+                `insert into parcela(parcela_cpf, valor_parcela, data_emprestimo, data_vencimento, numero_parcela, quant_parcelas, emprestimo_id) values($1, $2, $3, $4, $5, $6, $7)`,
+                {bind: [parcelas[i].cpf, parcelas[i].valorParcela, parcelas[i].dataEmprestimo, parcelas[i].dataVencimento, parcelas[i].numeroParcela, parcelas[i].quantParcelas, emprestimo_id], 
                 type: QueryTypes.INSERT}
             );
         }
